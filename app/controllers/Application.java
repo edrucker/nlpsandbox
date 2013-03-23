c1e65c0bf15a588e019c92b86e0af2fe3dfadac1
Eric Drucker <edrucker6@gmail.com>
2013-03-23 19:09:36 -0400

    Moved echonest service call to Akka actor
    Moved echonest call to Akka actor to allow for significant processing on
    results rather than using nested WS calls

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,15 +4,24 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.node.ObjectNode;
 import org.codehaus.jackson.node.ArrayNode;
 
+import static akka.pattern.Patterns.ask;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+
 import play.*;
+import play.libs.Akka;
 import play.libs.F.Function;
 import play.libs.Json;
 import play.libs.WS;
+import play.libs.WS.Response;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
+	
+	final static ActorRef songAnalyzer = SongAnalyzer.instance;
   
     public static Result index() {
         return ok(index.render("Your new application is ready."));
@@ -20,43 +29,17 @@ public class Application extends Controller {
     
     public static Result analyzeSong(String title, String artist) {
     	
-    	Result result;
+    	String tempString = title + ":::" + artist; //TODO use JSON object and pass to actor
     	
-    	if(title.equals(""))
-    	{
-    		ObjectNode errorNode = getErrorJSON("Missing parameter 'title'");
-    		result = badRequest(errorNode);
-    	}
-    	else if (artist.equals(""))
-    	{
-    		ObjectNode errorNode = getErrorJSON("Missing parameter 'artist'");
-    		result = badRequest(errorNode);
-    	}	
-    	else
-    	{
-    		ObjectNode resultNode = Json.newObject();
-    		
-    		//Get song id from EchoNest API
-    		
-    		return async(
-    			WS.url("http://developer.echonest.com/api/v4/song/search")
-    				.setQueryParameter("api_key", "6QHT9QEQUS9A7BCPW")
-    				.setQueryParameter("title", title)
-    				.setQueryParameter("artist", artist)
-    				.get().map(
-    					new Function<WS.Response, Result>() {
-    						public Result apply(WS.Response response) {
-    							return ok(response.asJson());
-    						}
-    					}
-    				)
-    			 
-    		);
-    		
-    		//result = ok(resultNode);
-    	}
-    	
-    	return result;
+    	return async(
+    		    Akka.asPromise(ask(songAnalyzer, tempString, 1000)).map(	//using 1000ms timeout
+    		      new Function<Object,Result>() {
+    		        public Result apply(Object response) {
+    		          return ok(response.toString());
+    		        }
+    		      }
+    		    )
+    		  );
     }
     
     //Convert current error to JSON node
@@ -65,6 +48,32 @@ public class Application extends Controller {
     	ObjectNode errorNode = Json.newObject();
     	errorNode.put("error", error);
     	return errorNode;
+    }
+    
+    public static class SongAnalyzer extends UntypedActor {
+    	static ActorRef instance = Akka.system().actorOf(new Props(SongAnalyzer.class));
+
+		@Override
+		public void onReceive(Object message) {
+			if (!(message instanceof String))
+			{
+				getContext().sender().tell(TODO, instance);
+			}
+			else
+			{
+				String title = "Creep";
+				String artist = "Radiohead";
+				
+				 Response response = WS.url("http://developer.echonest.com/api/v4/song/search")
+				.setQueryParameter("api_key", "6QHT9QEQUS9A7BCPW")
+				.setQueryParameter("title", title)
+				.setQueryParameter("artist", artist)
+				.get().get();
+				
+				getContext().sender().tell(response.asJson(), instance);
+			}
+			
+		}
     }
   
 }
