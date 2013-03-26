d032a1d747b1012846d190ceb46fb8856d8d08ac
Eric Drucker <edrucker6@gmail.com>
2013-03-26 07:56:44 -0400

    Added Track model

---
+++ app/models/Track.java
@@ -0,0 +1,28 @@
+package models;
+
+import play.db.ebean.*;
+import play.data.validation.Constraints.*;
+
+import javax.persistence.*;
+
+@Entity
+public class Track extends Model{
+	
+	@Id
+	public Long id;
+	
+	@Required
+	public String name;
+	
+	@Required
+	public String artist;
+	
+	@Required
+	public String lyrics;
+	
+	public static Finder<Long, Track> find = 
+			new Finder(Long.class, Track.class);
+	
+	
+	
+}
