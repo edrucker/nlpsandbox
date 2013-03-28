71cacf187985d036964b0fbca2efe316bbda3b29
Eric Drucker <edrucker6@gmail.com>
2013-03-28 16:59:38 -0400

    Added TextRazor named entity recognition

--- app/controllers/TextRazorNamedEntityRecognition.java
+++ app/controllers/TextRazorNamedEntityRecognition.java
@@ -2,6 +2,11 @@ package controllers;
 
 import org.codehaus.jackson.node.ObjectNode;
 
+import play.libs.Json;
+import play.libs.WS;
+import play.libs.WS.Response;
+import play.mvc.Http.Status;
+
 public class TextRazorNamedEntityRecognition implements NamedEntityRecognition
 {
 
@@ -9,6 +14,16 @@ public class TextRazorNamedEntityRecognition implements NamedEntityRecognition
 	public ObjectNode getNamedEntities(String text)
 	{
 		ObjectNode namedEntities = null;
+		
+		Response response = WS.url("http://api.textrazor.com/")
+				.setContentType("application/x-www-form-urlencoded")
+				.post("apiKey=788449c98bc6e8c35acb08e9993118be4547021441074a592d4eece9&extractors=topics,entities&text=" + text).get();
+		
+		if(response.getStatus() == Status.OK)
+		{
+			namedEntities = Json.newObject();
+			namedEntities.put("textrazor-named-entities", response.asJson());	
+		}
 		
 		return namedEntities;
 	}
